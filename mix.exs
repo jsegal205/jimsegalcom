6f1e7331bf634bb080cb6a12a4fae435dadcb874
Jim Segal <hello@jimsegal.com>
2023-10-31 13:46:02 -0500

    fix format

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Jimsegalcom.MixProject do
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
