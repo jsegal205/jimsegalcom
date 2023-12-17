f66443d467dd3dd3ee6cb5ea5c6ac2a26f8afb02
Jim Segal <hello@jimsegal.com>
2023-12-17 17:05:15 -0600

    add logger

--- lib/mix/tasks/echo.ex
+++ lib/mix/tasks/echo.ex
@@ -4,8 +4,11 @@ defmodule Mix.Tasks.Echo do
 
   use Mix.Task
 
+  import Logger
+
   @impl Mix.Task
   def run(args) do
+    Logger.warning("echo task #{Enum.join(args, " ")}")
     Mix.shell().info(Enum.join(args, " "))
   end
 end
