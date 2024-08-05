92b1747561a1fec5c44027c1ee6fe74ced8be985
Jim Segal <hello@jimsegal.com>
2024-08-04 20:34:19 -0500

    add tzdata

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule Jimsegalcom.MixProject do
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"}
+      {:telemetry_poller, "~> 1.0"},
+      {:tzdata, "~> 1.1"}
     ]
   end
 
