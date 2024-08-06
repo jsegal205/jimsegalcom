2455b9f7fecb01229ebaaa98da2de720f7fe0376
Jim Segal <hello@jimsegal.com>
2024-08-06 09:58:17 -0500

    add current_temp api call, refactor some too

--- test/jimsegalcom/api/weather_test.exs
+++ test/jimsegalcom/api/weather_test.exs
@@ -36,4 +36,34 @@ defmodule Jimsegalcom.Api.WeatherTest do
       assert Weather.max_temp(lat: 1, lon: 2) == {:ok, %{max_temp: 34.56}}
     end
   end
+
+  describe "current_temp/1" do
+    @bad_request_msg "`lat` or `lon` params missing"
+
+    test "errors when no opts passed" do
+      assert capture_log([level: :error], fn ->
+               assert Weather.current_temp() == {:error, @bad_request_msg}
+             end) =~ @bad_request_msg
+    end
+
+    test "errors when no lat passed" do
+      assert capture_log([level: :error], fn ->
+               assert Weather.current_temp(lon: 1) == {:error, @bad_request_msg}
+             end) =~ @bad_request_msg
+    end
+
+    test "errors when no lon passed" do
+      assert capture_log([level: :error], fn ->
+               assert Weather.current_temp(lat: 1) == {:error, @bad_request_msg}
+             end) =~ @bad_request_msg
+    end
+
+    test "calls api" do
+      Req.Test.stub(Weather, fn conn ->
+        Req.Test.json(conn, %{"main" => %{"temp" => 34.56}})
+      end)
+
+      assert Weather.current_temp(lat: 1, lon: 2) == {:ok, %{current_temp: 34.56}}
+    end
+  end
 end
